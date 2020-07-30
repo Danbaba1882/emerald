2e937a8bc3b8d073df2d63568607a4cc9eb874f8
engrdanbaba <usmandanbaba306@gmail.com>
2020-07-30 17:53:22 +0100

    add initial commit

--- models/database.js
+++ models/database.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb://127.0.0.1:27017/EMERALD'
+const URI = 'mongodb://127.0.0.1:27017/EMERALD';
+MONGODB_URI = 'mongodb+srv://danbaba1882:NAZIR1882@megaincomedb-wqmxa.mongodb.net/EMERALD';
 const mongoOptions={useNewUrlParser : true, useUnifiedTopology: true};
-mongoose.connect(URI, mongoOptions);
+mongoose.connect(process.env.MONGODB_URI || URI, mongoOptions);
 mongoose.connection.on('open',()=>{
     console.log('server has been connected to database');
     });
